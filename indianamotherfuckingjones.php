627653c24fd15a6a43b020526937ec6af9115680
Neoteknon <63339984+Neoteknon@users.noreply.github.com>
2020-11-02 17:57:37 +0100

    Create indianamotherfuckingjones.php

---
+++ indianamotherfuckingjones.php
@@ -0,0 +1,32 @@
+<?php
+
+$weapons = ['fist', 'whip', 'gun'];
+$opponentWeapon = $weapons[rand(0,2)]; // Cela permet de choisir une arme de manière aléatoire.
+echo 'Ops Indiana... The opponent\'s weapon of choice is the ' . $opponentWeapon . '. What will you do about it, ehn?' . '... A, a, americafirst, ahaa. no problems. Min>
+
+switch ($opponentWeapon){
+        case $weapons[0]:
+        echo $theWeaponOfIndianaMotherOfFuckinStupidsJones = $weapons[2];
+        break;
+
+        case $weapons[1]:
+        echo $theWeaponOfIndianaMotherOfFuckinStupidsJones = $weapons[0];
+        break;
+
+        case $weapons[2]:
+        echo $theWeaponOfIndianaMotherOfFuckinStupidsJones = $weapons[1];
+        break;
+}
+
+echo ' ... 3... 2... 1... Fight.';
+// $theWeaponOfIndianaMotherOfFuckinStupidsJones is $indyWeapon
+// joy
+
+?>
+
+
+Output:
+
+root@kali:/home/XXX/WildC/phpra/pub# php indianasjustayankee.php 
+Ops Indiana... The opponent's weapon of choice is the whip. What will you do about it, ehn?... A, a, americafirst, ahaa. no problems. Mine is the fist ... 3... 2... 1... Fight.
+
