09345e54d104b996909cacce729771fc593d218c
Neoteknon <63339984+Neoteknon@users.noreply.github.com>
2020-11-02 18:41:23 +0100

    Create deplug.php

---
+++ deplug.php
@@ -0,0 +1,25 @@
+<?php
+$characters = [
+    "ro"=> [
+        'city'=> 'sigma',
+        'weapon'=> 'tao'
+    ],
+    "Negan" => [
+        "city" => "The Sanctuary",
+        "weapon" => "Lucille"
+    ],
+    "Daryl" => [
+        "city" => "Alexandria",
+        "weapon" => "crossbow"
+    ],
+    "Ezekiel" => [
+        "city" => "The Kingdom",
+        "weapon" => "Shiva"
+    ]
+];
+
+var_dump($characters);
+
+?>
+
+Screenshot link: https://ibb.co/svwMTdw
